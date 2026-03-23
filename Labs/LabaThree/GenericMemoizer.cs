549b2080ee0e7fb87cd15e6b1110f081200433bb
Oleksandr-C-S <oleksandrchakun@gmail.com>
2026-03-23 23:31:13 +0000

    add custom memoizer

---
+++ Labs/LabaThree/GenericMemoizer.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class GenericMemoizer<T, TResult>{
+
+    private readonly Func<T, TResult> _func;
+    private readonly int _capacity;
+    private readonly Func<Dictionary<T, TResult>, T> _evictStrategy;
+
+    private readonly Dictionary<T, TResult> _cache = new();
+
+    public GenericMemoizer(
+
+        Func<T, TResult> func,
+        int capacity,
+        Func<Dictionary<T, TResult>, T> evictStrategy)
+    {
+        _func = func;
+        _capacity = capacity;
+        _evictStrategy = evictStrategy;
+        
+    }
+
+
+
+    
+}
