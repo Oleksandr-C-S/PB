21fec549be2f0c21b554d7f6c35e7d3a2ab4a7bb
Oleksandr-C-S <oleksandrchakun@gmail.com>
2026-04-22 23:02:56 +0000

    add Cancellation and Sequential version

--- Labs/LabaFive/AsyncFilter.cs
+++ Labs/LabaFive/AsyncFilter.cs
@@ -42,6 +42,40 @@ class AsyncFilter
         }
     );
     Console.WriteLine("Result " + string.Join(", ", resultAsync));
-    
+    //Senquential version
+    Console.WriteLine("Senquential version");
+    var resultSeq = await AsyncFilter.FilterSequentialAsync(
+        numbers,
+        async num =>
+        {
+            await Task.Delay(100);
+            return num > 5;
+        }
+    );
+    Console.WriteLine("Result " + string.Join(", ", resultSeq));
+
+    //Cancellation version
+    Console.WriteLine("Cancellation version");
+
+    var cts = new CancellationToken();
+    var task = AsyncFilter.FilterAsync(
+        numbers,
+        async num =>
+        {
+            await Task.Delay(200);
+            return num % 2 == 0;
+        },
+        cts.Token
+    );
+cts.CancelAfter(500);
+        try
+        {
+            var result = await task;
+            Console.WriteLine("Result "+ string.Join(", ", result));
+        }
+        catch(OperationCanceledException)
+        {
+            Console.WriteLine("Операцію скасовано");
+        }
     }
 }
