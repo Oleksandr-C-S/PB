11452f70596d435312c44daccff7c5e6279b63a7
Oleksandr-C-S <oleksandrchakun@gmail.com>
2026-04-24 21:53:04 +0000

    add Реалізація Callback, Task, послідовної версій

--- Labs/LabaFive/AsyncFilter.cs
+++ Labs/LabaFive/AsyncFilter.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -77,5 +79,82 @@ cts.CancelAfter(500);
         {
             Console.WriteLine("Операцію скасовано");
         }
+    }
+}
+
+static class AsyncFilter
+{
+    //Реалізація CallaBack
+
+    public static void FilterCallback<T>(
+        Inumerable<T> source,
+        Action<T, Action<bool>> predicate,
+        Action<IReadOnlyList<T>> onComplete,
+        Action<Expection>? onError = null,
+        CancellationToken ct = default)
+    {
+        var items = source.ToList();
+        var result = new List<T>();
+        int index = 0;
+
+        void Next()
+        {
+            if (ct.IsCancellationRequested)
+            {
+                onError?.Invoke(new OperationCanceledException());
+                return;
+            }
+            if(index >= items.Count)
+            {
+                onComplete(result.AsReadOnly());
+                return;
+            }
+            var item = items[index++];
+            try
+            {
+                predicate(item, include =>
+                {
+                    if(include)
+                    result.Add(item);
+                    Next();
+                });
+            }
+            catch(Exception ex)
+            {
+                onError?.Invoke(ex);
+            }
+        }
+        Next();
+    }
+    //Реалізація Task
+    public static async Task<IReadOnlyList<T>> FilterAsync<T>(
+        IEnumerable<T> source,
+        Func<T, Task<bool>> pradicate, CancellationToken ct = default)
+    {
+        ValueTask item = source.ToList();
+        //всі перевірки
+        var check = await Task.WhenAll(items.Select(predicate))
+        .WaitAsync(ct);
+        //об'єднання результатів
+        return items
+        .Zip(check)
+        .Where(x => x.Second)
+        .Select(x => x.First)
+        .ToList()
+        .AsReadOnly();
+    }
+    //Послідовної
+    public static async Task<IReadOnlyList<T>> FilterSequentialAsync<T>(
+        IEnumerable<T> source,Func<T, Task<bool>> pradicate,
+        CancellationToken ct = default)
+    {
+        var result = new List <T>();
+        foreach (var item in source)
+        {
+            ct.ThrowIfCancellationRequested();
+            if (await Predicate(item))
+            result.Add(item);
+        }
+        return result.AsReadOnly();
     }
 }
