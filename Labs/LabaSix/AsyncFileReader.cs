bd80563e2c8997c373e0af2f604191b5414213d6
Oleksandr-C-S <oleksandrchakun@gmail.com>
2026-05-10 14:37:16 +0000

    add comments

--- Labs/LabaSix/AsyncFileReader.cs
+++ Labs/LabaSix/AsyncFileReader.cs
@@ -1,15 +1,15 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
+using System.Collections.Generic;//Бібліотека для асинхронної роботи
+using System.IO;//Бібліотека для роботи з файлами
+using System.Threading.Tasks;//Бібліотека для Task
 
 namespace LabaSix
 {
-    public static class AsyncFileReader
+    public static class AsyncFileReader//Клас асинхронного читання файлу
     {
-        public static async IAsyncEnumerable<string> ReadLinesAsync(string path)
+        public static async IAsyncEnumerable<string> ReadLinesAsync(string path)//Асинхронний ітератор
         {
-            using StreamReader reader = new StreamReader(path);
-            string line;
+            using StreamReader reader = new StreamReader(path);//Для асинхронного читанню файлу
+            string line;//Зберігання поточного рядка
             while((line = await reader.ReadLineAsync()) != null)
             {
                 yield return line;
